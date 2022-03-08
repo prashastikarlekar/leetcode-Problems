cc4f5153f79a17935c3dedeb945a132adf84af8f
Prashasti Karlekar <prashastikarlekar@gmail.com>
2022-03-07 23:49:49 -0500

    Update 1.recursive- LCS.py

--- Data Structures/Dynamic Programming/1.recursive- LCS.py
+++ Data Structures/Dynamic Programming/1.recursive- LCS.py
@@ -3,7 +3,7 @@ def longestCommonSubsequence(A,B,i,j):
     if i==len(A) or j ==len(B):
         return 0
     elif A[i] == B[j]:
-        return 1+ longestCommonSubsequence(A,B,i+1,j+1) # ye to equal ho gaya so add 1 and then move to the next row and column
+        return 1+ longestCommonSubsequence(A,B,i+1,j+1)     
     else:
         return max(longestCommonSubsequence(A,B,i+1,j),longestCommonSubsequence(A,B,i,j+1))
     
